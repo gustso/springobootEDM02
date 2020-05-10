dfe39c479c0669e1e409a1360d14d729f7751be2
NTGustso <NTGustso@DESKTOP-0IA820K>
2020-05-10 15:56:44 -0700

    Inicializando Repositorio

---
+++ src/main/java/ar/edu/unju/edm/Tp04OtroSosaGustavoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04OtroSosaGustavoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04OtroSosaGustavoApplication.class, args);
+	}
+
+}
